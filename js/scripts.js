a58e793eb24856108e22e99813433deff1095165
Christopher Neal <christopher.m.neal@gmail.com>
2021-10-22 13:32:15 -0700

    add winner function

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,16 @@ function tallyResults(selection) {
   }
 }
 
+function winner(a, b, c) {
+  if (a > b && a > c) {
+    return a;
+  } else if (b > a && b > c) {
+    return b;
+  } else {
+    return c;
+  } //add function to determine tiebreaker by coin toss?
+}
+
 //user interface logic
 $(document).ready(function() {
   $("#quiz").submit(function(event) {
