c3520099931da01c9c896bbe6b104be39a9ddb0f
Christopher Neal <christopher.m.neal@gmail.com>
2021-10-22 15:12:12 -0700

    BUGFIX logic now working, WIP add images

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,97 @@
-// //business logic
-// let ruby = 0;
-// let csharp = 0;
-// let js = 0;
+let ruby = 0;
+let csharp = 0;
+let js = 0;
 
-// function tallyResults(selection) {
-//   if (selection === ruby) {
-//     ruby += 1;
-//   } else if (selection === csharp) {
-//     csharp += 1;
-//   } else if (selection === js) {
-//     js += 1;
-//   }
-// }
+function tallyResults(selection) {
+  if (selection === "ruby") {
+    ruby += 1;
+  } else if (selection === "csharp") {
+    csharp += 1;
+  } else if (selection === "js") {
+    js += 1;
+  }
+}
 
-// function winner(a, b, c) {
-//   if (a > b && a > c) {
-//     return a;
-//   } else if (b > a && b > c) {
-//     return b;
-//   } else {
-//     return c;
-//   } //add function to determine tiebreaker by coin toss?
-// }
+  function winner(a, b, c) {
+    if (a > b && a > c) {
+      return "Ruby";
+    } else if (b > a && b > c) {
+      return "C#"
+    } else {
+      return "JavaScript"
+    }
+  }
 
-//user interface logic
-// function setClass(winnerLang) {
-//   $("body").removeClass();
-//   if (winnerLang === ruby) {
-//     $("#result-output").append("<img src='ruby.jpeg'>");
-//     $("body").addClass("ruby");
-//   } else if (winnerLang === csharp) {
-//     $("#result-output").append("<img src='csharp.jpeg'>");
-//     $("body").addClass("csharp");
-//   } else {
-//     $("#result-output").append("<img src='js.jpg'>");
-//     $("body").addClass("js");
-//   }
-// }
+function setClass(winnerLang) {
+  $("body").removeClass();
+  $(".jumbotron").removeClass("ruby csharp js");
+  $(".card").removeClass("ruby csharp js");
+  if (winnerLang === "Ruby") {
+    $("#result-output").append("<img src='ruby.jpeg'>");
+    $("body").addClass("ruby");
+    $(".jumbotron").addClass("ruby");
+    $(".card").addClass("ruby");
+  } else if (winnerLang === "C#") {
+    $("#result-output").append("<img src='csharp.jpeg'>");
+    $("body").addClass("csharp");
+    $(".jumbotron").addClass("csharp");
+    $(".card").addClass("csharp");
+  } else {
+    $("#result-output").append("<img src='js.jpg'>");
+    $("body").addClass("js");
+    $(".jumbotron").addClass("js");
+    $(".card").addClass("js");
+  }
+}
 
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     event.preventDefault();
     
     const name = $("#name").val();
-    // tallyResults($("input:radio[name=hobby]:checked").val());
-    // tallyResults($("input:radio[name=tv]:checked").val());
-    // tallyResults($("input:radio[name=exercise]:checked").val());
-    // tallyResults($("input:radio[name=app1]:checked").val());
-    // tallyResults($("input:radio[name=app2]:checked").val());
-    // tallyResults($("input:radio[name=app3]:checked").val());
+    tallyResults($("input:radio[name=hobby]:checked").val());
+    tallyResults($("input:radio[name=tv]:checked").val());
+    tallyResults($("input:radio[name=exercise]:checked").val());
+    tallyResults($("input:radio[name=app1]:checked").val());
+    tallyResults($("input:radio[name=app2]:checked").val());
+    tallyResults($("input:radio[name=app3]:checked").val());
 
     $("#name-output").text(name);
-    // const lang = winner(ruby, csharp, js);
-    // $("#lang-output").text(lang);
-    // setClass(lang);
-    $("#result").show();
+    const lang = winner(ruby, csharp, js);
+    $("#lang-output").text(lang);
+    setClass(lang);
+    $("#results").show();
     
+    ruby = 0;
+    csharp = 0;
+    js = 0;
     //delay(400).slideDown(1200);
 
     //Add % of each function?
+
     
   });
 });
 
 
-/*
-on submit I want to:
-gather info
-rn tally function
-hobby
-tv
-exercise
-app1
-app2
-app3
-show results
 
-*/
+
+
+/* add function to determine tiebreaker by coin toss?
+
+change body class function
+function setClass(winnerLang) {
+  $("body").removeClass();
+  if (winnerLang === ruby) {
+    $("#result-output").append("<img src='ruby.jpeg'>");
+    $("body").addClass("ruby");
+  } else if (winnerLang === csharp) {
+    $("#result-output").append("<img src='csharp.jpeg'>");
+    $("body").addClass("csharp");
+  } else {
+    $("#result-output").append("<img src='js.jpg'>");
+    $("body").addClass("js");
+  }
+}
+
+} */
